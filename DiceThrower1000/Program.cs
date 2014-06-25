e47f968b5c94699debaddb326f9c614830e0da66
Zach Urbach <urbach-zach@gmail.com>
2014-06-25 15:25:27 -0600

    throw some dice

---
+++ DiceThrower1000/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceThrower1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //time to throw some dice
+
+
+
+
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
