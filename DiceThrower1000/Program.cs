5b949c9fcdb517b3398dee597db4744a25664ed3
Zach Urbach <urbach-zach@gmail.com>
2014-06-25 16:17:06 -0600

    added it all

--- DiceThrower1000/Program.cs
+++ DiceThrower1000/Program.cs
@@ -11,14 +11,28 @@ namespace DiceThrower1000
         static void Main(string[] args)
         {
             //time to throw some dice
-
-
-
+            diceString("10d6");
+            Console.WriteLine();
+            diceString("3d20");
+            Console.WriteLine();
+            diceString("100d6");
 
             Console.ReadKey();
         }
-
-
+        static void diceString(string input)
+        {
+            List <string> inputList = input.Split('d').ToList();
+            int n = int.Parse(inputList[0]);
+            int m = int.Parse(inputList[1]);
+            Random randomNumberGenerator = new Random();
+           
+            for (int i = 1; i < n; i += 1)
+			{
+			    int randomNumber = randomNumberGenerator.Next(1,m + 1);
+                Console.WriteLine(randomNumber);
+            }
+            
+        }
 
     }
 }
