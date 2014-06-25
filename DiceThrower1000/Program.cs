5e7b65dd6873b938f87f365a5009c469ef02551a
Zach Urbach <urbach-zach@gmail.com>
2014-06-25 16:31:08 -0600

    added the last bits
    added comments and a final tweak

--- DiceThrower1000/Program.cs
+++ DiceThrower1000/Program.cs
@@ -16,19 +16,23 @@ namespace DiceThrower1000
             diceString("3d20");
             Console.WriteLine();
             diceString("100d6");
-
+            //keeps console open
             Console.ReadKey();
         }
         static void diceString(string input)
         {
             List <string> inputList = input.Split('d').ToList();
+            //in "10d6" 10 is the [0]
+            // and 6 is the [1]
             int n = int.Parse(inputList[0]);
             int m = int.Parse(inputList[1]);
+
             Random randomNumberGenerator = new Random();
            
-            for (int i = 1; i < n; i += 1)
+            for (int i = 1; i < n + 1; i += 1)
 			{
-			    int randomNumber = randomNumberGenerator.Next(1,m + 1);
+                //the nifty randome number generator
+			    int randomNumber = randomNumberGenerator.Next(1, m + 1);
                 Console.WriteLine(randomNumber);
             }
             
